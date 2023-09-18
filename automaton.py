042d5308c930634e97064393b1b6283634e7837f
larmen9179 <lo.j.armendariz@gmail.com>
2023-09-18 13:00:45 -0600

    Completed user string loop to check acceptance

--- automaton.py
+++ automaton.py
@@ -1,3 +1,5 @@
+
+
 print("Welcome to the Automaton checker!")
 print("First, we'll ask for all the states and symbols and how they work together...")
 print("Then you can enter in characters and check if your automaton accepts it!!")
@@ -19,6 +21,13 @@ while (not done):
     else:
         states.append(option)
 
+#Declaring starting and ending states
+s0 = input("Which of these states is the starting state? :")
+
+s0 = states.index(s0)
+print(s0)
+sf = input("Which of these states is the ending state? : ")
+sf = states.index(sf)
 
 #Re-declaring done as false so we can now check symbols
 done = False
@@ -39,8 +48,33 @@ print("Now we'll ask you how the states transition with symbols")
 for i in range(len(states)):
     transitionsToAdd = []
     for j in range(len(symbols)):
-        nextState = input("If we are on state " + states[i] " and we see symbol " + symbols[i] + " , what is the next states we go to: ")
+        nextState = input("If we are in state " + states[i] + " and we see symbol " + symbols[j] + " , what is the next state we go to: ")
         transitionsToAdd.append(nextState)
     
     transitions.append(transitionsToAdd)
+
+#Printing the matrix for output
+for i in range(len(transitions)):
+    print(transitions[i])
+
+
+#Checking if the input is read by the automata
+print("Now we'll check if your input is accepted by this automaton...")
+
+userString = input("Enter in your string: ")
+userArray = list(userString)
+
+#Outputting the users string for comparison
+print(userArray)
+
+
+currentState = s0
+
+for i in range(len(userArray)):
+    currentState = states[currentState][symbols.index(userArray[i])]
+
+if currentState != sf:
+    print("The user's input was not accepted :(")
+else:
+    print("The user's input was accepted! :)")
 
